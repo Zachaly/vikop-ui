42ab6cd7c1958273757510e583ad29f5bc39c6ca
Zachary Soja <zachary3003@gmail.com>
2022-10-09 01:55:33 +0200

    feature: add finding pagination

---
+++ src/components/Pages.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav class="pagination" role="navigation" aria-label="pagination">
+        <a class="pagination-previous" @click="changePage(currentPage - 1)" v-if="currentPage != 1">
+            Previous
+        </a>
+        <a class="pagination-next" @click="changePage(currentPage + 1)" v-if="currentPage != pageCount">
+            Next page
+        </a>
+        <ul class="pagination-list" >
+            <li v-for="page in pageCount" :key="page">
+                <a :class="{'is-current': page === currentPage, 'pagination-link': true}" aria-label="Page 1" aria-current="page" @click="changePage(page)">
+                    {{page}}
+                </a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'pages-comp'
+}
+</script>
+
+<script setup>
+import { defineProps, ref, defineEmits } from 'vue';
+
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({
+    pageCount: Number
+})
+
+const currentPage = ref(1)
+const emit = defineEmits(['change-page'])
+
+function changePage(page){
+    currentPage.value = page
+    emit('change-page', page)
+}
+
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,9 @@
   <Tabs :tabs="['Hot', 'New']" @change-tab="changeTab"/>
   <div v-if="!loading">
     <Finding class="mt-2" v-for="finding in findings.value" :key="finding.id" :finding="finding"/>
+    <Pages class="mt-2 mb-5" :pageCount="pages" @change-page="changePage"/>
   </div>
+  
 </template>
 
 <script setup>
@@ -11,15 +13,18 @@ import { onMounted, reactive, ref } from '@vue/runtime-core';
 import axios from 'axios';
 import { useAuthStore } from '@/stores/authStore';
 import Tabs from '@/components/Tabs.vue';
+import Pages from '@/components/Pages.vue'
 
 const findings = reactive([])
 const loading = ref(true)
 const store = useAuthStore()
 const tab = ref(0)
+const pages = ref(0)
+const currentPage = ref(0)
 
 function getTopFindings(){
   loading.value = true
-  axios.get('finding/hot')
+  axios.get('finding/hot/' + currentPage.value)
     .then(res => {
       findings.value = res.data
     }).then(() => {
@@ -35,7 +40,7 @@ function getTopFindings(){
 
 function getNewFindings(){
   loading.value = true
-  axios.get('finding/new')
+  axios.get('finding/new/' + currentPage.value)
     .then(res => {
       findings.value = res.data
     }).then(() => {
@@ -52,16 +57,30 @@ function getNewFindings(){
 
 function changeTab(number){
   if(number === 0){
-    getTopFindings()
+    getTopFindings(0)
   }
   else if (number === 1){
-    getNewFindings()
+    getNewFindings(0)
   }
 
   tab.value = number
 }
 
+function changePage(page){
+  currentPage.value = page - 1
+
+  if(tab.value === 0){
+    getTopFindings()
+  }
+  else if(tab.value === 1){
+    getNewFindings()
+  }
+}
+
 onMounted(() => {
+  axios.get('finding/pagecount').then(res => {
+    pages.value = res.data
+  })
   getTopFindings()
 })
 
